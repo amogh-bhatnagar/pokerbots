697965f8290fa0f551a545467ea22ca30dd52b53
amogh-bhatnagar <amoghb@mit.edu>
2021-01-15 22:02:45 -0600

    Fixed some stuffs

--- executable.sh
+++ executable.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Just type in terminal ./executable.sh
 > out.txt
-for VAR in 1 2 3 4 5, 6
+for VAR in 1 2 3 4 5 6
 do  
     python3 engine.py
     grep -i 'Final' gamelog.txt >> out.txt
